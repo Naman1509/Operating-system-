4a41d8fc0d3966df58abf666999ba24110c97a44
Naman jain <54211708+Naman1509@users.noreply.github.com>
2020-03-31 22:55:18 +0530

    Add files via upload

---
+++ part2.c
@@ -0,0 +1,34 @@
+ for(i=0; i<n; i++)  
+   {  
+     ct[i] = -1;  
+     ta[i] = bt[i];  
+   }  
+   while(pc!=n)  
+   {  
+     c = 0;  
+     for(i=0; i<n; i++)  
+       if(ct[i]<0 && at[i]<=tn)  
+         c++;  
+     if(c==0)  
+       tn++;  
+     else  
+     {  
+       pointer = 0;  
+       while(at[pointer]>tn || ct[pointer]>0)  
+         pointer++;  
+       for(k=pointer+1; k<n; k++)  
+         if(at[k]<=tn && ct[k]<0 && bt[pointer]>bt[k])  
+           pointer = k;  
+       if(ct[pointer]<0)  
+       {  
+         tn=tn+bt[pointer];  
+         bt[pointer] = 0;  
+         ct[pointer] = tn;  
+         wt[pointer] = ct[pointer] - ( at[pointer]+ ta[pointer] );  
+         tat[pointer] = ct[pointer] - at[pointer]; 
+        
+          
+         pc++;  
+       }  
+     }  
+   }
