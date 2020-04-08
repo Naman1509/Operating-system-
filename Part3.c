af11089693cbcc3d19233d0c8c083650b0edd7e0
Naman jain <54211708+Naman1509@users.noreply.github.com>
2020-04-08 22:23:14 +0530

    Update Part3.c

--- Part3.c
+++ Part3.c
@@ -1,4 +1,7 @@
- for(i=0;i<n;i++)
+// Calculating Total Waiting Time And Total Turn Around Time And Average Waiting Time  and Average Total Turn Around Time
+
+
+for(i=0;i<n;i++)
    {
    	totwt += wt[i];
    	totta += tat[i];
