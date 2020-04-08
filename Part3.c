69ad5b8ccba2a8841e8ee4d6ee5d7599d2414480
Naman jain <54211708+Naman1509@users.noreply.github.com>
2020-04-08 22:21:15 +0530

    Add files via upload

---
+++ Part3.c
@@ -0,0 +1,10 @@
+ for(i=0;i<n;i++)
+   {
+   	totwt += wt[i];
+   	totta += tat[i];
+   }
+   printf("total time : %d",totwt);
+   
+   
+    awt=(float)totwt/(float)n;
+    ata=(float)totta/(float)n; 
