4151be584d44833898d3a156752e021b5b0d9a38
Naman jain <54211708+Naman1509@users.noreply.github.com>
2020-04-13 23:49:53 +0530

    Add files via upload

---
+++ Part4.c
@@ -0,0 +1,17 @@
+// Calculating Total extra time taken by the process  
+     
+    xtra = 2*(n-1);
+    xtratime = burstadd + xtra;
+    
+   printf("\nPN\tAT\tBT\tCT\tWT\tTAT\n");  
+   for(i=0;i<n;i++) 
+      {
+       printf("%s\t%d\t%d\t%d\t%d\t%d\n",pn[i],at[i],ta[i],ct[i],wt[i],tat[i]);
+      
+      }
+     
+	printf("\n average turnaround time is : %f \n",ata);
+	printf("\n average waiting time is : %f", awt); 
+	printf("\n The total time taken by the processor to compute all the jobs is : %d ", xtratime); 
+   return 0;  
+ }  
